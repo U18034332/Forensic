b6ce5f5ee8af70d6e5f2d769ebf18ed2756930a2
Elias Makhonjwa <126951032+U18034332@users.noreply.github.com>
2024-04-09 14:39:37 +0200

    Added create report dto

---
+++ src/main/kotlin/com/nlc/forensic/dto/FundingIncidentReportDTO.kt
@@ -0,0 +1,16 @@
+package com.nlc.forensic.dto
+
+data class FundingIncidentReportDTO(
+    val projectNumber: String,
+    var province: String,
+    var caseType: String,
+    var channel: String,
+    var priority: String,
+    var status: String,
+    var organisation: String,
+    var detectionSource: String,
+    var description: String,
+    var sector: String,
+    var detectionLevel: String,
+    var detectionDivision: String
+)
