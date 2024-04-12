517b0474b474c11bcf88a4e246549d6e32349a89
Elias Makhonjwa <126951032+U18034332@users.noreply.github.com>
2024-04-12 10:41:49 +0200

    Added logic to add and dismiss a modal

--- client/src/app/dto/funding-incident-report.interface.ts
+++ client/src/app/dto/funding-incident-report.interface.ts
@@ -1,17 +1,16 @@
 export interface FundingIncidentReport {
-    startDate: string,
-    dateReported: string,
-    projectNumber: string,
-    province: string,
-    caseType: string,
-    channel: string,
-    priority: string,
-    status: string,
-    organisation: string,
-    detectionSource: string,
-    description: string,
-    sector: string,
-    detectionLevel: string,
-    detectionDivision: string,
-    assessmentStage: number
+    startDate: string;
+    dateReported: string;
+    projectNumber: string;
+    province: string;
+    caseType: string;
+    channel: string;
+    priority: string;
+    status: string;
+    organisation: string;
+    sourceDetection: string;
+    sector: string;
+    levelDetected: string;
+    allocatedDescription: string;
+    divisionDetected: string;
 }
--- client/src/app/pages/incident-report/add-report/add-report.page.ts
+++ client/src/app/pages/incident-report/add-report/add-report.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { FundingIncidentReport } from 'src/app/dto/funding-incident-report.interface';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-report-form', // ensure this selector is correct
@@ -7,15 +9,21 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./add-report.page.scss'],
 })
 export class AddReportPage {
-  report: any = {};
+  constructor(private modal: ModalController) { }
+  report: FundingIncidentReport = {} as FundingIncidentReport
 
   // Function to handle form submission - Renamed to match the HTML template
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    console.log(form.value)
+    console.log(this.report)
+    this.modal.dismiss()
   }
 
   // Function to handle cancel action - Added to match the HTML template
   onCancel() {
-    console.log('Cancel action triggered');
+    console.log('Cancel action triggered')
+    this.report = {} as FundingIncidentReport
+    this.modal.dismiss()
+    
   }
 }
