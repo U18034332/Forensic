07af2895a3871b98db20bfacdeaae679897c290b
Elias Sibongiseni Makhonjwa <Elias.Makhonjwa@nlcsa.org.za>
2024-05-23 15:01:58 +0200

    Cleaned the code

---
+++ client/src/app/dto/report-case.interface.ts
@@ -0,0 +1,22 @@
+export interface ReportCase {
+    reportNumber: string;
+    acceptance: string;
+    dateReported: string;
+    channel: string;
+    province: string;
+    caseType: string;
+    priority: string;
+    status: string;
+    detectionDivision: string;
+    levelDetected: string;
+    assessmentStage: string;
+    startDate: string;
+    reportDocument: string;
+    actions: string;
+    lastModified: string;
+    assignedToo: string;
+    projectNumber: string;
+    organisation: string;
+    stage: string;
+    assessedBy: string;
+  }
