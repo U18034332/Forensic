4cc19393802dea531e006281bc29d233ee9dafe5
Elias Sibongiseni Makhonjwa <Elias.Makhonjwa@nlcsa.org.za>
2024-06-20 10:07:31 +0200

    Fixed some login issues with styling issues

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -3,43 +3,33 @@ import { Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from '../services/auth-service.service';
 
-
 @Injectable({
   providedIn: 'root'
 })
 
-
 @Component({
   selector: 'app-login',
-  
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
-  private email: string = '';
-  private password: string = '';
-  private token: string = ''
-  
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
 
   constructor(private router: Router, private authService: AuthService) {}
 
   login() {
-    
     this.authService.login(this.email, this.password).subscribe(
       response => {
-        this.token = response.token
-        
-        console.log(this.token);
-
+        console.log(response);
         this.router.navigate(['/dashboard']);
-        
       },
       error => {
         console.log(error);
-        
-
+        this.errorMessage = 'Login failed. Please check your email and password.';
       }
     );
   }
 }
+
