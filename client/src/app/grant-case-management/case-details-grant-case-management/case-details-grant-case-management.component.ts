a9e53ae89a346f6aba4cec3096d678d0a84d2274
Bailey.Swanepoel <Bailey.Swanepoel@nlcsa.org.za>
2024-07-15 15:33:30 +0200

    Merged changes from teamate and added my work

--- client/src/app/grant-case-management/case-details-grant-case-management/case-details-grant-case-management.component.ts
+++ client/src/app/grant-case-management/case-details-grant-case-management/case-details-grant-case-management.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogConfig, } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig, } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { AnnexureADigitalFormComponent } from '../annexures-grant/annexure-a-digital-form/annexure-a-digital-form.component';
 
 @Component({
@@ -10,33 +11,21 @@ import { AnnexureADigitalFormComponent } from '../annexures-grant/annexure-a-dig
 export class CaseDetailsGrantCaseManagementComponent {
     case: any; // Define the type of your case data here
 
-    constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialog: MatDialog) {}
-  
-    presentAddAnnexureADialog(): void {
-      const dialogConfig = new MatDialogConfig();
-  
-      // Initial configuration for dialog
-      dialogConfig.width = '80%';
-      dialogConfig.maxHeight = '90vh';
-      dialogConfig.panelClass = 'custom-dialog-container';
-  
-      // Open the dialog with initial configuration
-      const dialogRef = this.dialog.open(AnnexureADigitalFormComponent, dialogConfig);
-  
-      // After dialog is opened, adjust its position if needed
-      dialogRef.afterOpened().subscribe(() => {
-        const dialogContainer = dialogRef.componentInstance.dialogRef['_containerInstance'];
-  
-        // Ensure dialogContainer and _config are defined before accessing properties
-        if (dialogContainer && dialogContainer['_config']) {
-          dialogContainer['_config'].position = {
-            left: '50px',  // Adjust left position
-            top: '50px'    // Adjust top position as needed
-          };
-  
-          dialogRef.updatePosition();  // Update dialog position
-        }
-      });
-    }
+    constructor(private router: Router, private dialog: MatDialog) {}
+
+  openAnnexureA() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '45%';
+    dialogConfig.maxHeight = '90vh';
+    // dialogConfig.panelClass = 'custom-dialog-container';
+
+    const dialogRef = this.dialog.open(AnnexureADigitalFormComponent, dialogConfig);
   }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
+}
+
+  
   
