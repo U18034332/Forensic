c79ee10ea224d32ad69d7020deef2357982181e5
Jade-NLC <Jade.Klynsmith@nlcsa.org.za>
2024-07-16 13:36:22 +0200

    User and Firm Table Update

--- client/src/app/incident-report/funding-reports/funding-reports.component.ts
+++ client/src/app/incident-report/funding-reports/funding-reports.component.ts
@@ -8,11 +8,12 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class FundingReportsComponent implements OnInit {
   fundingReports = new MatTableDataSource([
-    { reportid: 1, decision: 'not recommended', datereported: '2024-01-01', user: 'User 1', firm: 'firm 1', reason: 'why', status: 'closed', channel: 'Hotline',casetype: 'Theft', priority: 'Simple' },
+    { reportid: 1, decision: 'not recommended', datereported: '2024-01-01', userorfirm: 'user or firm' , reason: 'why', status: 'closed', channel: 'Hotline',casetype: 'Theft', priority: 'Simple' },
     
     // Add more data as needed
   ]);
-  displayedColumns: string[] = ['reportid', 'decision','user','firm','reason', 'datereported','status','channel','casetype','priority',];
+  displayedColumns: string[] = ['reportid', 'decision','userorfirm','reason', 'datereported','status','channel','casetype','priority',];
+
 
 
   constructor() {}
