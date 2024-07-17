a1eb0fc9ac57fc003bab0ad200883c8fe6d7fdfd
Bailey.Swanepoel <Bailey.Swanepoel@nlcsa.org.za>
2024-07-17 10:31:00 +0200

    Digital Forms&Notification

--- client/src/app/calendar/calendar.component.ts
+++ client/src/app/calendar/calendar.component.ts
@@ -1,24 +1,114 @@
 import { Component } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import { MatDialog,MatDialogConfig } from '@angular/material/dialog';
+import { CalendarOptions, EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+import { CalendarEventDialogComponent } from './calendar-event-dialog/calendar-event-dialog.component';
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
 export class CalendarComponent {
+  events: EventInput[] = [
+    { id: '1', title: 'Event 1', start: '2024-07-16', end: '2024-07-18' },
+    { id: '2', title: 'Event 2', start: '2024-07-17T10:00:00', end: '2024-07-17T12:00:00' }
+  ];
+
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    plugins: [dayGridPlugin]
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    events: this.events,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventDrop: this.handleEventChange.bind(this),
+    eventResize: this.handleEventChange.bind(this),
+    dateClick: this.handleDateClick.bind(this)
   };
 
-  handleDateClick(eventInfo: any) {
-    console.log('Date clicked', eventInfo.dateStr);
-    // Handle date click logic here
+  constructor(public dialog: MatDialog) {}
+
+  handleDateClick(arg: any) {
+    const dialogRef = this.dialog.open(CalendarEventDialogComponent, {
+      width: '500px',
+      data: { start: arg.dateStr, end: arg.dateStr }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        const newEvent = {
+          id: String(Date.now()),
+          title: result.title,
+          start: result.start,
+          end: result.end,
+          description: result.description
+        };
+        this.events = [...this.events, newEvent];
+        this.calendarOptions.events = [...this.events];
+      }
+    });
   }
 
-  handleEventClick(eventClickInfo: any) {
-    console.log('Event clicked', eventClickInfo.event.title);
-    // Handle event click logic here
+  handleEventClick(arg: any) {
+    alert('Event clicked: ' + arg.event.title);
+  }
+
+  handleDateSelect(selectInfo: any) {
+    const dialogRef = this.dialog.open(CalendarEventDialogComponent, {
+      width: '45%',
+      data: { start: selectInfo.startStr, end: selectInfo.endStr }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        const newEvent = {
+          id: String(Date.now()),
+          title: result.title,
+          start: result.start,
+          end: result.end,
+          description: result.description
+        };
+        this.events = [...this.events, newEvent];
+        this.calendarOptions.events = [...this.events];
+      }
+    });
+  }
+
+  handleEventChange(changeInfo: any) {
+    alert('Event changed: ' + changeInfo.event.title);
+  }
+
+  openEventDialog(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '45%';
+    dialogConfig.maxHeight = '90vh';
+    // dialogConfig.panelClass = 'custom-dialog-container';
+
+    const dialogRef = this.dialog.open(CalendarEventDialogComponent, dialogConfig);
+  
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        const newEvent = {
+          id: String(Date.now()),
+          title: result.title,
+          start: result.start,
+          end: result.end
+        };
+        this.events = [...this.events, newEvent];
+        this.calendarOptions.events = [...this.events];
+      }
+    });
   }
 }
--- client/src/app/core/tool-bar/toolbar.component.ts
+++ client/src/app/core/tool-bar/toolbar.component.ts
@@ -1,10 +1,26 @@
 import { Component, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSidenav } from '@angular/material/sidenav';
+import { NotificationSidePanelComponent } from './notification-side-panel/notification-side-panel.component';
+
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
+  @ViewChild('drawer') drawer!: MatSidenav;
 
+  constructor(private router: Router, private dialog: MatDialog) {}
+
+  openNotificationSidePanel() {
+    const dialogRef = this.dialog.open(NotificationSidePanelComponent, {
+      width: '30%'
+    });
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 }
