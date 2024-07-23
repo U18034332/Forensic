25ae6a49122dbff6237b9f916c451ef586482ac8
Elias Makhonjwa <u18034332@tuks.co.za>
2024-07-23 10:51:18 +0200

    Integrated eveluated report for funding related not recommended

--- client/src/app/incident-report/assessment-funding/assessment-funding.component.ts
+++ client/src/app/incident-report/assessment-funding/assessment-funding.component.ts
@@ -4,6 +4,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { FundingRelatedFormComponent } from '../funding-related-form/funding-related-form.component';
 import { InvestigationDialogComponent } from '../investigation-dialog/investigation-dialog.component';
 import { NotRecommendedDialogComponent } from '../not-recommended-dialog/not-recommended-dialog.component';
+import { IncidentReportEvaluation } from '../../models/incident-report-evaluation';
+import { FundingRelatedService } from '../../services/funding-related.service';
+import { IncidentReportComponent } from '../incident-report.component';
 @Component({
   selector: 'app-assessment-funding',
   templateUrl: './assessment-funding.component.html',
@@ -20,6 +23,8 @@ export class AssessmentFundingComponent {
 
   constructor(
     public dialog: MatDialog,
+    private incidentAssessmentService: FundingRelatedService,
+    private incidentReportComponent: IncidentReportComponent
   ) {}
 
 
@@ -39,14 +44,27 @@ export class AssessmentFundingComponent {
       // Handle the result here (e.g., navigate to a specific page or perform an action based on the selection)
     });
   }
-  openNotRecommendedDialog(): void {
+  openNotRecommendedDialog(element: any): void {
     const dialogRef = this.dialog.open(NotRecommendedDialogComponent);
+    console.log(element);
+    
     
 
     dialogRef.afterClosed().subscribe(result => {
       if (result.action === 'submit') {
+        const evaluation: IncidentReportEvaluation = {
+          reportNumber: element.reportNumber,
+          allocateTo: '',
+          declineReason: result.reason
+        };
+        this.incidentAssessmentService.assessIncidentReport(evaluation)
+          .subscribe((res)=>{
+            this.incidentReportComponent.reloadReports()
+            console.log(res);
+          }, (err)=>{
+            console.log('Error assessing incident report:', err);
+          })
         console.log('Reason for not recommending:', result.reason);
-        // Handle the reason here (e.g., save to the database or perform other actions)
       }
     });
   }
