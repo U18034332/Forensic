55231130958a47162ddbacca9a39825d9524cf28
Mash <munashemash71@gmail.com>
2024-07-24 11:09:58 +0200

    initial

--- client/src/app/doc-management/doc-management.module.ts
+++ client/src/app/doc-management/doc-management.module.ts
@@ -7,23 +7,16 @@ import { MatCardModule } from '@angular/material/card';
 import { MatCardContent } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CoreModule } from '../core/core.module';
 
-@NgModule({
-    declarations: [
+@NgModule({ declarations: [
         DocManagementComponent,
-        
-    ],
-    imports: [
-        CommonModule,
+    ], imports: [CommonModule,
         DocManagementComponentRoutingModule,
         MatCardModule,
         MatProgressBarModule,
         MatButtonModule,
         MatIconModule,
-        HttpClientModule,
-        CoreModule
-    ]
-})
+        CoreModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class DocManagementModule { }
