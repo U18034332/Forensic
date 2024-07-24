8899a5c3575adb3c0a236102dff7c40074a23ed2
Elias Makhonjwa <u18034332@tuks.co.za>
2024-07-24 11:07:20 +0200

    Created the backend api enpoint to get all the users and admins. Also fixed the frontend user selection dialog to allocate to one user. And created the service to get all the user and admins for integatrion

--- src/main/kotlin/com/nlc/forensic/enums/AcceptanceStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.nlc.forensic.enums
-
-enum class AcceptanceStatus(private val status: String) {
-    ACCEPTED("Accept"),
-    DECLINED("Declined");
-    fun getValue(): String {
-        return status
-    }
-}
